07502f2ef2d27c65a4aa2fe9febc6b99f314751a
susiejojo <dipanwita.g@research.iiit.ac.in>
2019-06-09 22:48:11 +0530

    Images work

--- blog-app/index.js
+++ blog-app/index.js
@@ -4,12 +4,14 @@ const expressEdge=require('express-edge');
 const app = new express();
 const mongoose=require("mongoose");
 const bodyParser=require("body-parser");
+const fileUpload=require("express-fileupload");
 const Post=require('./database/models/Post')
 
 mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
     .then(() => 'You are now connected to Mongo!')
     .catch(err => console.error('Something went wrong', err))
     
+app.use(fileUpload());
 app.use(express.static('public'));
 app.use(expressEdge);
 app.use(bodyParser.json())
@@ -26,9 +28,18 @@ app.get('/', async (req, res) => {
 app.get('/new', (req, res) => {
     res.render('create');
 });
-app.post('/posts/store', (req, res) => {
-    Post.create(req.body,(error,post)=>{
-    	res.redirect('/')
+app.post("/posts/store", (req, res) => {
+    const {
+        image
+    } = req.files
+ 
+    image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
+        Post.create({
+            ...req.body,
+            image: `/posts/${image.name}`
+        }, (error, post) => {
+            res.redirect('/');
+        });
     })
 });
 app.get('/about', (req, res) => {
