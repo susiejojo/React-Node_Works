152302ede002235739bcb883f5035fc7c17c384c
susiejojo <dipanwita.g@research.iiit.ac.in>
2019-06-09 22:32:39 +0530

    Able to create posts

--- blog-app/index.js
+++ blog-app/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const expressEdge=require('express-edge');
 const app = new express();
 const mongoose=require("mongoose");
+const bodyParser=require("body-parser");
+const Post=require('./database/models/Post')
 
 mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
     .then(() => 'You are now connected to Mongo!')
@@ -10,9 +12,24 @@ mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true 
     
 app.use(express.static('public'));
 app.use(expressEdge);
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.set('views',__dirname+"/views");
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const posts=await Post.find({})
+    res.render('index',{
+    	posts
+    })
+});
+app.get('/new', (req, res) => {
+    res.render('create');
+});
+app.post('/posts/store', (req, res) => {
+    Post.create(req.body,(error,post)=>{
+    	res.redirect('/')
+    })
 });
 app.get('/about', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'pages/about.html'));
@@ -20,11 +37,11 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
 });
-app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
-});
-app.get('/posts/new', (req, res) => {
-    res.render('create');
+app.get('/:id', async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    res.render('post', {
+        post
+    })
 });
 app.listen(4000, () => {
     console.log('App listening on port 4000');
