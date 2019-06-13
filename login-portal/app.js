379f4b6e5710f0304931f94d0383acf47a12847d
susiejojo <dipanwita.g@research.iiit.ac.in>
2019-06-14 00:56:47 +0530

    Working OAuth

--- login-portal/app.js
+++ login-portal/app.js
@@ -1,23 +1,24 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-//var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session=require('express-session');
 const dashboardRouter = require("./routes/dashboard");         
 const publicRouter = require("./routes/public");
+const usersRouter=require("./routes/users");
 var okta=require('@okta/okta-sdk-nodejs');
 var ExpressOIDC=require("@okta/oidc-middleware").ExpressOIDC;
 
 var app = express();
 var oktaClient = new okta.Client({
-  orgUrl: 'student-dev-406160',
+  orgUrl: 'https://dev-406160.okta.com',
   token: '00ERLVHvYQAy8YUzK1tCVGK1ABGchcdBZ_EHJuoYCv'
 });
 const oidc = new ExpressOIDC({
-  issuer: "student-dev-406160/oauth2/default",
-  client_id: 0oapx9jk5YYXdHH2u356,
-  client_secret: i7kPLSXdKXkth7L7WhH8XNdF3dg4kkmu8oDlKb2M,
+  issuer: "https://dev-406160.okta.com/oauth2/default",
+  client_id: '0oapx9jk5YYXdHH2u356',
+  client_secret: 'i7kPLSXdKXkth7L7WhH8XNdF3dg4kkmu8oDlKb2M',
   redirect_uri: 'http://localhost:3000/users/callback',
   scope: "openid profile",
   routes: {
@@ -38,15 +39,38 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'susieandteddyrcute',
   resave: true,
   saveUninitialized: false
 }));
+app.use(oidc.router);
+app.use((req, res, next) => {
+  if (!req.userinfo) {
+    return next();
+  }
+
+  oktaClient.getUser(req.userinfo.sub)
+    .then(user => {
+      req.user = user;
+      res.locals.user = user;
+      next();
+    }).catch(err => {
+      next(err);
+    });
+});
+function loginRequired(req, res, next) {
+  if (!req.user) {
+    return res.status(401).render("unauthenticated");
+  }
+
+  next();
+}
 app.use('/', publicRouter);
-app.use('/users', dashboardRouter);
+app.use('/dashboard', loginRequired, dashboardRouter);
+app.use('/users',usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
