cd3a91e0146560a22e481cbe81d46c72000c114c
susiejojo <dipanwita.g@research.iiit.ac.in>
2019-06-14 00:12:10 +0530

    New app

--- blog-app/controllers/homePage.js
+++
@@ -1,9 +0,0 @@
-const Post = require('../database/models/Post')
- 
-module.exports = async (req, res) => {
-    const posts = await Post.find({});
- 
-    res.render("index", {
-        posts
-    });
-}
--- blog-app/middleware/storePost.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-    if (!req.body.username || !req.body.title || !req.body.description || !req.body.content) {
-        return res.redirect('/new')
-    }
- 
-    next()
-}
